d48fb0d38146c817b2f2efe7e4c0acc86002e43a
s2210 <s2210tai@gmail.com>
2022-03-08 09:05:44 +0300

    path change

--- common.h
+++ common.h
@@ -9,7 +9,7 @@ using std::filesystem::current_path;
 
 namespace Path {
 const QString project_root = QString::fromStdString(current_path().string()) + "/../../";
-const QString paths_Yaml = "/home/pc/Desktop/SpaceLoader/Setup/Paths.yaml";
+const QString paths_Yaml = project_root + "Setup/Paths.yaml";
 const QString bin_Dir = project_root + QString::fromStdString(Yaml::getText<std::string>(Yaml::getNodeByTag<YAML::Node>(paths_Yaml.toStdString(), "bin_Dir"), "bin_Dir"));
 const QString server_cmds_Yaml = project_root + QString::fromStdString(Yaml::getText<std::string>(Yaml::getNodeByTag<YAML::Node>(paths_Yaml.toStdString(), "server_cmds_Yaml"), "server_cmds_Yaml"));
 const QString client_cmds_Yaml = project_root + QString::fromStdString(Yaml::getText<std::string>(Yaml::getNodeByTag<YAML::Node>(paths_Yaml.toStdString(), "client_cmds_Yaml"), "client_cmds_Yaml"));
