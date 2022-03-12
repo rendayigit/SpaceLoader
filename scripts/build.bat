e44b8fb9c92d0f7758bfe0325534b006c37d26a1
Renda Yigit <renda.yigit@gmail.com>
2022-03-12 16:25:29 +0300

    Modified build scripts

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,3 +1,4 @@
+SET PATH=%PATH%;C:\msys64\mingw64\bin\
 CALL ./scripts/clean.bat
 cmake -E make_directory ./build
 cmake --build ./build --config Debug --target all -j 18 --
--- scripts/clean.bat
+++ scripts/clean.bat
@@ -1,1 +1,2 @@
+SET PATH=%PATH%;C:\msys64\mingw64\bin\
 cmake --build ./build --config Debug --target clean
