a48b125777bd6e5a9249ec1ae05a474aad6989a8
Renda <renda.yigit@gmail.com>
2022-03-12 16:48:55 +0300

    updated clean scripts

--- scripts/clean.bat
+++ scripts/clean.bat
@@ -1,2 +1,5 @@
 SET PATH=%PATH%;C:\msys64\mingw64\bin\
 cmake --build ./build --config Debug --target clean
+rm -rf "build/"
+rm -rf ".cache/"
+rm -rf "bin/"
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,1 +1,4 @@
 cmake --build ./build --config Debug --target clean
+rm -rf build
+rm -rf .cache
+rm -rf bin
