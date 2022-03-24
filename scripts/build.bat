7b6dee89d96fecc6a873432677bbdf2782003efc
Renda <renda.yigit@gmail.com>
2022-03-24 22:19:31 +0300

    fixed build script for windows

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,9 +1,8 @@
 SET PATH=%PATH%;C:\msys64\mingw64\bin\
 
-cmake -E make_directory ./build
+mkdir %~dp0..\build\
 
-@REM TODO - This command doesn't work
-cmake -DCMAKE_PREFIX_PATH:STRING=C:/msys64/mingw64/lib/cmake/ -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_CC_COMPILER:FILEPATH=C:/msys64/mingw64/bin/gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:/msys64/mingw64/bin/g++.exe -S. -B/build -G "MinGW Makefiles"
+cmake -DCMAKE_PREFIX_PATH:STRING=C:/msys64/mingw64/lib/cmake/ -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_CC_COMPILER:FILEPATH=C:/msys64/mingw64/bin/gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:/msys64/mingw64/bin/g++.exe -S%~dp0..\ -B%~dp0..\build\ -G "MinGW Makefiles"
 
 set /a coreCount=%NUMBER_OF_PROCESSORS% + 2
-cmake --build ./build --config Debug --target all -j %coreCount% --
+cmake --build %~dp0..\build\ --config Debug --target all -j %coreCount% --
