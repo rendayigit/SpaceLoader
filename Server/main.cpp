367c4aa4832b6b55780a0540b055e04025316dcd
busra.balaban <balabanbsr@gmail.com>
2022-04-01 15:19:08 +0300

    main.cpp reverted

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,16 +1,12 @@
-#include <iostream>
-#include "../common.h"
 #include "server.h"
-#include <stdlib.h>
 
-int main(int argc, char *argv[]){
-
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
-    
+
     Server::getInstance()->populateCmdLists();
+
     qint32 serverPort = 1234;
     Server::getInstance()->startServer(serverPort);
 
-   return a.exec();
-    
+    return a.exec();
 }
