cc2e1579680d97785596e959575498d7090736ab
Renda Yigit <renda.yigit@gmail.com>
2022-04-20 00:10:04 +0300

    Fixed command tests

--- tests/Commands/test_commands.cpp
+++ tests/Commands/test_commands.cpp
@@ -2,12 +2,32 @@
 
 #include "../../Commands/hello/hello.h"
 #include "../../common.h"
+#include "../../lib/Logger/logger.h"
 #include "../Test_common.h"
 
 TEST(Commands, hello) {
     QTcpSocket *testSocket{};
 
-    runDynamicCmd(testSocket, "hello");
+    CmdPlugin *testPlugin = new CmdPlugin();
+    testPlugin->run(testSocket, "hello");
 
-    /* TODO: Read logs and see if string ("Ran Hello !!") exists */
+    /* Read logs and see if string ("Ran Hello !!") exists */
+    QFile file(Log()->getLogDir() + Log()->getLogFileName());
+
+    if (file.exists()) {
+        file.open(QIODevice::ReadOnly);
+    } else {
+        GTEST_FAIL() << "Log file not found: " +
+                            (Log()->getLogDir() + Log()->getLogFileName()).toStdString();
+    }
+
+    bool pass = false;
+    QString line;
+    while (!file.atEnd()) {
+        if (file.readLine().contains("Ran Hello !!")) {
+            pass = true;
+        }
+    }
+
+    EXPECT_TRUE(pass);
 }
