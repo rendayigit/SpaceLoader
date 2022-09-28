f8e4c275c84c4324c2bc62ae43fc0f86d14f67d9
rendayigit <renda.yigit@gmail.com>
2022-09-28 11:05:12 +0300

    Minor fixes

--- Client_gui/listener.h
+++ Client_gui/listener.h
@@ -6,25 +6,20 @@
 
 class Listener : public TCPClient {
    public:
+    explicit Listener(Backend *backend) { this->backend = backend; };
+    ~Listener() override = default;
+
     Listener(const Listener &) = delete;
     Listener &operator=(Listener const &) = delete;
     Listener(Listener &&) = delete;
     Listener &operator=(Listener &&) = delete;
-    ~Listener() = default;
 
-    static auto &getInstance() {
-        static Listener instance;
-        return instance;
-    }
-
-    explicit Listener(Backend* backend) { this->backend = backend; };
     void onReceived(QByteArray message) override;
     void disconnect();
     void setConnected() { connected = true; }
 
    private:
-    Listener();
-    Backend* backend;
+    Backend *backend;
     bool connected = false;
 };
 
