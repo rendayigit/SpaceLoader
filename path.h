c6ae2e7fd9326b0f34bf2a375b4e024e74f31863
Renda <renda.yigit@gmail.com>
2023-07-26 22:25:27 +0300

    paths refactoring

--- path.h
+++ path.h
@@ -6,41 +6,101 @@
 
 #include "lib/YAML/yaml.h"
 
-#define Paths Path::getInstance
+namespace Path {
+/**
+ * Returns the path of the executable file.
+ *
+ * @return The path of the executable file.
+ */
+[[nodiscard]] static QString getExecutablePath() {
+    QString bin = QCoreApplication::applicationFilePath();
+    int lastSlashIndex = bin.lastIndexOf('/');
+    return bin.left(lastSlashIndex + 1);
+}
 
-class Path {
-   public:
-    [[nodiscard]] static QString getExecutablePath() {
-        QString bin =
-            QString::fromStdWString(QCoreApplication::applicationFilePath().toStdWString());
-        return bin.mid(0, bin.lastIndexOf(QChar('/')) + 1);
-    }
+/**
+ * Retrieves the root directory of the project.
+ *
+ * @return The root directory of the project.
+ */
+[[nodiscard]] static QString getProjectRoot() {
+    QString executablePath = getExecutablePath();
+    QString projectRoot = executablePath + "../";
+    return projectRoot;
+}
 
-    [[nodiscard]] static QString getProjectRoot() { return getExecutablePath() + "../"; }
+/**
+ * Retrieves the setup directory.
+ *
+ * @return The setup directory as a QString
+ */
+[[nodiscard]] static QString getSetupDir() {
+    QString executablePath = getExecutablePath();
+    QString setupDir = executablePath + "Setup/";
+    return setupDir;
+}
 
-    [[nodiscard]] static QString getSetupDir() { return getExecutablePath() + "Setup/"; }
+/**
+ * Retrieves the path to the YAML file containing the paths.
+ *
+ * @return The path to the YAML file.
+ */
+[[nodiscard]] static QString getPathsYaml() {
+    QString setupDir = getSetupDir();
+    QString pathsYaml = setupDir + "Paths.yaml";
+    return pathsYaml;
+}
 
-    [[nodiscard]] static QString getPathsYaml() { return getSetupDir() + "Paths.yaml"; }
+/**
+ * Retrieves the directory path for the commands.
+ *
+ * @return The directory path for the commands.
+ */
+[[nodiscard]] static QString getCmdsDir() {
+    std::string pathsYaml = getPathsYaml().toStdString();
+    std::string cmdLibDir = Yaml::getValue(pathsYaml, "cmd_lib_Dir");
+    QString setupDir = getSetupDir();
+    QString cmdsDir = setupDir + QString::fromStdString(cmdLibDir);
+    return cmdsDir;
+}
 
-    [[nodiscard]] static QString getCmdsDir() {
-        return getSetupDir() +
-               QString::fromStdString(Yaml::getValue(getPathsYaml().toStdString(), "cmd_lib_Dir"));
-    }
+/**
+ * Retrieves the path to the server commands YAML file.
+ *
+ * @return The path to the server commands YAML file.
+ */
+[[nodiscard]] static QString getServerCmdsYaml() {
+    QString setupDir = getSetupDir();
+    std::string pathsYaml = getPathsYaml().toStdString();
+    std::string serverCmdsYaml = Yaml::getValue(pathsYaml, "server_cmds_Yaml");
+    return setupDir + QString::fromStdString(serverCmdsYaml);
+}
 
-    [[nodiscard]] static QString getServerCmdsYaml() {
-        return getSetupDir() + QString::fromStdString(Yaml::getValue(getPathsYaml().toStdString(),
-                                                                     "server_cmds_Yaml"));
-    }
+/**
+ * Retrieves the YAML path for client commands.
+ *
+ * @return the YAML path for client commands as a QString.
+ */
+[[nodiscard]] static QString getClientCmdsYaml() {
+    std::string clientCmdsYamlPath = getPathsYaml().toStdString();
+    std::string clientCmdsYamlKey = "client_cmds_Yaml";
+    std::string clientCmdsYamlValue = Yaml::getValue(clientCmdsYamlPath, clientCmdsYamlKey);
+    QString setupDir = getSetupDir();
+    QString clientCmdsYaml = QString::fromStdString(clientCmdsYamlValue);
+    return setupDir + clientCmdsYaml;
+}
 
-    [[nodiscard]] static QString getClientCmdsYaml() {
-        return getSetupDir() + QString::fromStdString(Yaml::getValue(getPathsYaml().toStdString(),
-                                                                     "client_cmds_Yaml"));
-    }
-
-    [[nodiscard]] static QString getConfigYaml() {
-        return getSetupDir() +
-               QString::fromStdString(Yaml::getValue(getPathsYaml().toStdString(), "config_Yaml"));
-    }
-};
+/**
+ * Retrieves the configuration YAML file path.
+ *
+ * @return the path to the configuration YAML file
+ */
+[[nodiscard]] static QString getConfigYaml() {
+    std::string setupDir = getSetupDir().toStdString();
+    std::string pathsYaml = getPathsYaml().toStdString();
+    std::string configYaml = Yaml::getValue(pathsYaml, "config_Yaml");
+    return QString::fromStdString(setupDir + configYaml);
+}
+};  // namespace Path
 
 #endif  // PATH_H
